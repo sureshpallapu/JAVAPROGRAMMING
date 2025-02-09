4c33faac121971e5baaf6c733fd92880859025cc
sureshpallapu <pallapusuresh2000@gmail.com>
2025-02-09 10:58:58 +0530

    pattern

---
+++ JAVAPROGRAMMING/src/PATTERNPROGRAMS/square/AlphabetROW.java
@@ -0,0 +1,24 @@
+package PATTERNPROGRAMS.square;
+
+import java.util.Scanner;
+
+public class AlphabetROW {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the number u want");
+		int n;
+		n = s.nextInt();
+		char a = 'A';
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j <= n; j++) {
+				System.out.print(a + " ");
+
+			}
+			a++;
+			System.out.println();
+		}
+	}
+
+}
