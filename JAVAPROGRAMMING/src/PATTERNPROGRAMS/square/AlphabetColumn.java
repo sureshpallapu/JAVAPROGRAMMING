0abd97e3df4724410157623a475b1d4db0d82fca
sureshpallapu <pallapusuresh2000@gmail.com>
2025-02-09 10:58:42 +0530

    pattern

---
+++ JAVAPROGRAMMING/src/PATTERNPROGRAMS/square/AlphabetColumn.java
@@ -0,0 +1,25 @@
+package PATTERNPROGRAMS.square;
+
+import java.util.Scanner;
+
+public class AlphabetColumn {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the number u want");
+		int n;
+		n=s.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			char a='A';
+		for(int j=1;j<=n;j++) {
+			System.out.print(a+" ");
+			a++;
+		}
+		
+		System.out.println();
+		}
+
+	}
+
+}
