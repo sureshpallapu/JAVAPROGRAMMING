366a84ab4f161b70df05fd13afcd02050bb1c3c1
sureshpallapu <pallapusuresh2000@gmail.com>
2025-02-11 09:41:14 +0530

    new

---
+++ JAVAPROGRAMMING/src/PATTERNPROGRAMS/square/BinarySquareRowstart1.java
@@ -0,0 +1,16 @@
+package PATTERNPROGRAMS.square;
+
+public class BinarySquareRowstart1 {
+	 public static void main(String[] args) {
+		 int n = 5; 
+		 System.out.println("Binary Pattern:");
+		 for (int i = 1; i <= n; i++) { 
+		 for (int j = 1; j <= n; j++) { 
+		 System.out.print((j % 2) + " ");
+		 
+		}
+		 System.out.println(); 
+		 
+		}
+	 }
+}
