619abcd4e03bffcb9d403dfe8e92feda187d7e7b
sureshpallapu <pallapusuresh2000@gmail.com>
2025-02-15 20:34:24 +0530

    new

---
+++ JAVAPROGRAMMING/src/NUMBERPROGRAMS/EvenOdd/EvenorOdd.java
@@ -0,0 +1,24 @@
+package NUMBERPROGRAMS.EvenOdd;
+
+import java.util.Scanner;
+
+public class EvenorOdd {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the number ");
+		int i = s.nextInt();
+		if (i % 2 == 0) {
+			System.out.println(i + " is even number ");
+		} else {
+			System.out.println(i + " is odd number ");
+		}
+		// method 2
+
+		String r = (i % 2 == 0) ? i+"  is even number " : i+  " is odd number ";
+		System.out.println(r);
+		
+		// method 3
+		String[] res= {"is even number "," is  odd number"};
+		System.out.println(i+" "+res[i%2]);
+	}
+}
