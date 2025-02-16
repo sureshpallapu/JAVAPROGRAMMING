0b5b22b15b53943febf7b3563d1e4a163aff4f91
sureshpallapu <pallapusuresh2000@gmail.com>
2025-02-16 18:12:33 +0530

    new

---
+++ JAVAPROGRAMMING/src/NUMBERPROGRAMS/FactorsCount.java
@@ -0,0 +1,20 @@
+package NUMBERPROGRAMS;
+
+import java.util.Scanner;
+
+public class FactorsCount {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the number ");
+		int n = s.nextInt();
+		int factor = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				System.out.println(i + " is a factor");
+				factor++;
+			}
+
+		}
+		System.out.println(" total Factors : " + factor);
+	}
+}
