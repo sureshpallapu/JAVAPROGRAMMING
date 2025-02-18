69b1293430c70b8433aa64dc42d31b5e3cb57dec
sureshpallapu <pallapusuresh2000@gmail.com>
2025-02-18 15:53:36 +0530

    new

---
+++ JAVAPROGRAMMING/src/STRINGPROGRAMS/InBulitMethodsPrograms/CompareStringsIgnoreCase.java
@@ -0,0 +1,16 @@
+package STRINGPROGRAMS.InBulitMethodsPrograms;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class CompareStringsIgnoreCase {
+    public static void main(String[] args) {
+        String s1 = "apple";
+        String s2 = "Apple";
+
+        System.out.println(s1.compareToIgnoreCase(s2)); // Output: 0 (ignores case)
+        
+        
+        
+    }
+}
