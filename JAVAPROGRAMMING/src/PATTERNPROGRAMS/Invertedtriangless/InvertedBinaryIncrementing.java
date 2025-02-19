9bc91cbb716b365cb0dca5559c19c9f45de0d65a
sureshpallapu <pallapusuresh2000@gmail.com>
2025-02-19 21:20:47 +0530

    new

---
+++ JAVAPROGRAMMING/src/PATTERNPROGRAMS/Invertedtriangless/InvertedBinaryIncrementing.java
@@ -0,0 +1,19 @@
+package PATTERNPROGRAMS.Invertedtriangless;
+
+import java.util.Scanner;
+
+public class InvertedBinaryIncrementing {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the number u want");
+		int n;
+		n = s.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			for (int k = i; k <= n; k++) {
+				System.out.print((k % 2) + "   ");
+			}
+			System.out.println();
+		}
+	}
+}
