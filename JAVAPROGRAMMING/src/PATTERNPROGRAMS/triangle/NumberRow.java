eb6247a78255188cb3f5f021e9362a00914f9767
sureshpallapu <pallapusuresh2000@gmail.com>
2025-02-22 17:02:47 +0530

    new

---
+++ JAVAPROGRAMMING/src/PATTERNPROGRAMS/triangle/NumberRow.java
@@ -0,0 +1,23 @@
+package PATTERNPROGRAMS.triangle;
+
+import java.util.Scanner;
+
+public class NumberRow {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the number u want");
+		int n;
+		n = s.nextInt();
+		int a = 1;
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j <= i; j++) {
+				System.out.print(a + " ");
+			}
+			a++;
+			System.out.println();
+		}
+	}
+
+}
