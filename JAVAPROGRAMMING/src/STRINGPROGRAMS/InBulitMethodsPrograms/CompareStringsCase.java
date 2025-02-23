3e928d8b19c10b4a0136779fbe8416dded39868e
sureshpallapu <pallapusuresh2000@gmail.com>
2025-02-23 22:26:37 +0530

    NEW

--- JAVAPROGRAMMING/src/STRINGPROGRAMS/InBulitMethodsPrograms/CompareStringsCase.java
+++ JAVAPROGRAMMING/src/STRINGPROGRAMS/InBulitMethodsPrograms/CompareStringsCase.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class CompareStringsCase {
     public static void main(String[] args) {
         String s1 = "apple";
-        String s2 = "Applf";
+        String s2 = "pplf";
 
         System.out.println(s1.compareTo(s2)); // Output: 32 (because 'a' > 'A' in ASCII)
         
