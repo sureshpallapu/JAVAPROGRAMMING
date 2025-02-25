e1583051bb73d1b47754e1b85b3ad3ed6b82a4b8
sureshpallapu <pallapusuresh2000@gmail.com>
2025-02-25 21:10:10 +0530

    new

---
+++ JAVAPROGRAMMING/src/PATTERNPROGRAMS/ExtraPatterns/X_StarPattern.java
@@ -0,0 +1,31 @@
+package PATTERNPROGRAMS.ExtraPatterns;
+
+public class X_StarPattern {
+	
+	    public static void main(String[] args) {
+	        int n = 5; // Number of rows
+
+	        // Upper part
+	        for (int i = n; i >= 1; i--) {
+	            for (int j = n; j > i; j--) {
+	                System.out.print(" ");
+	            }
+	            for (int k = 1; k <= (2 * i - 1); k++) {
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+
+	        // Lower part
+	        for (int i = 2; i <= n; i++) {
+	            for (int j = n; j > i; j--) {
+	                System.out.print(" ");
+	            }
+	            for (int k = 1; k <= (2 * i - 1); k++) {
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+	    }
+	}
+
