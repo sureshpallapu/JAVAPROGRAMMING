6bdee7dcbf521bd7f0d5b4603c3379e1831b8cc4
sureshpallapu <pallapusuresh2000@gmail.com>
2025-02-28 20:44:55 +0530

    new

---
+++ JAVAPROGRAMMING/src/PATTERNPROGRAMS/triangle/StarRightTriangle.java
@@ -0,0 +1,21 @@
+package PATTERNPROGRAMS.triangle;
+
+import java.util.Scanner;
+
+public class StarRightTriangle {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the number u want");
+		int n;
+		n = s.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*  ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
