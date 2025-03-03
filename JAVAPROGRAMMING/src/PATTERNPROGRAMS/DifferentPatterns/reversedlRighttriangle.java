81d0a919de60f8cf79e53830cf7a9b177d504c8e
sureshpallapu <pallapusuresh2000@gmail.com>
2025-03-03 08:24:29 +0530

    new

---
+++ JAVAPROGRAMMING/src/PATTERNPROGRAMS/DifferentPatterns/reversedlRighttriangle.java
@@ -0,0 +1,24 @@
+package PATTERNPROGRAMS.DifferentPatterns;
+
+import java.util.Scanner;
+
+public class reversedlRighttriangle {
+		public static void main(String[] args) {
+			Scanner s = new Scanner(System.in);
+			System.out.println("enter the number u want");
+			int n;
+			n = s.nextInt();
+			  for (int i = n; i >= 1; i--) {  
+		            for (int j = 1; j <= n - i; j++) { 
+		                System.out.print("  ");
+		            }
+		            for (int k = 1; k <= i; k++) {
+		                System.out.print("*"+" ");		            		                
+		            }
+		            System.out.println();
+		        }
+		}
+
+	}
+
+
