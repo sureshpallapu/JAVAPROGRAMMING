122e5acbaff781fd2dde39666cf3bc1661ec07b0
sureshpallapu <pallapusuresh2000@gmail.com>
2025-03-03 08:24:44 +0530

    new

---
+++ JAVAPROGRAMMING/src/PATTERNPROGRAMS/ExtraPatterns/ReversedDoubleHilledStarPattern.java
@@ -0,0 +1,34 @@
+package PATTERNPROGRAMS.ExtraPatterns;
+
+public class ReversedDoubleHilledStarPattern {
+	
+
+	    public static void main(String[] args) {
+	        int n = 5; 
+	        for (int i = 1; i <= n; i++) {
+	            for (int j = 1; j < i; j++) {
+	                System.out.print(" ");
+	            }
+	            for (int j = i; j <= n; j++) {
+	                System.out.print("*");
+	            }
+	            for (int j = i; j < n; j++) {
+	                System.out.print("*");
+	            }
+	            for (int j = 1; j <= i - 1; j++) { 
+	                System.out.print(" ");
+	            }
+	            for (int j = 1; j <= i - 1; j++) { 
+	                System.out.print(" ");
+	            }
+	            for (int j = i; j <= n; j++) {
+	                System.out.print("*");
+	            }
+	            for (int j = i; j < n; j++) {
+	                System.out.print("*");
+	            }
+	            System.out.println(); 
+	        }
+	    }
+	}
+
