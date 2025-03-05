a9f139fdbcfe28bb24edff48288da925167d54dd
sureshpallapu <pallapusuresh2000@gmail.com>
2025-03-05 22:11:14 +0530

    new

---
+++ JAVAPROGRAMMING/src/PATTERNPROGRAMS/Pyramids/PyramidAlphaColumn.java
@@ -0,0 +1,30 @@
+package PATTERNPROGRAMS.Pyramids;
+
+import java.util.Scanner;
+
+public class PyramidAlphaColumn {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the number u want");
+		int n;
+		n = s.nextInt();
+	
+	   for (int i = 1; i <= n; i++) {
+			char a='A';
+		for (int k = i; k <= n; k++) {//de
+			System.out.print(" ");
+		}
+			for (int l = 1; l <= i; l++) {//in
+				System.out.print(a+" ");
+				a++;
+			}
+			for (int j = 1; j < i; j++) {//in
+				System.out.print(a+" ");
+				a++;
+			}		
+			System.out.println();
+
+		}
+
+	}
+}
