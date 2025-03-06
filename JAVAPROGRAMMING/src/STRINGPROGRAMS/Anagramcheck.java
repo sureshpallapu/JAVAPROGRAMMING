ed39f3f1c79d38ba19e73959083a0229a3e6f798
sureshpallapu <pallapusuresh2000@gmail.com>
2025-03-06 23:02:49 +0530

    new

--- JAVAPROGRAMMING/src/STRINGPROGRAMS/Anagramcheck.java
+++ JAVAPROGRAMMING/src/STRINGPROGRAMS/Anagramcheck.java
@@ -3,12 +3,11 @@ package STRINGPROGRAMS;
 public class Anagramcheck {
 	public static void main(String[] args) {
 		String s = "cat".replaceAll("\\s", "").toLowerCase();
-		String s1 = "a ct".replaceAll("\\s", "").toLowerCase();
+		String s1 = "acmt".replaceAll("\\s", "").toLowerCase();
 		int[] count = new int[256];
 		if (s.length() != s1.length()) {
 			System.out.println("not an anagaram");
 			return;
-
 		}
 		for (int c : s.toCharArray()) {
 			count[c]++;
@@ -17,11 +16,11 @@ public class Anagramcheck {
 			count[c]--;
 		}
 		for (int i = 0; i < count.length; i++) {
-			if (count[i] != 0) {
-				System.out.println("is not aanagaram");
+			if (count[i] == 0) {
+				System.out.println("is  aanagaram");
 				return;
 			}
 		}
-		System.out.println("is a anagaram");
+		System.out.println("is not a anagaram");
 	}
 }
