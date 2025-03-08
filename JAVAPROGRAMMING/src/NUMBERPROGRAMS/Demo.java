8bdb1f8086017b5068bc77757066ff9eb232b086
sureshpallapu <pallapusuresh2000@gmail.com>
2025-03-08 23:04:44 +0530

    new

---
+++ JAVAPROGRAMMING/src/NUMBERPROGRAMS/Demo.java
@@ -0,0 +1,24 @@
+package NUMBERPROGRAMS;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+//		int i = 0;
+//		while (i < 5) {
+//			if (i == 3)
+//				continue;
+//			System.out.println(i);
+//			i++;
+//		System.out.println("====int m = (Integer) null;=======");
+//		int m = (Integer) null;
+//		System.out.println("=====Integer i = null;======");
+     	Integer i = null;
+		 int j = i + 5;
+//		String s = null;
+//		int l = s.length();
+//		System.out.println(l);
+		// }
+	}
+
+}
