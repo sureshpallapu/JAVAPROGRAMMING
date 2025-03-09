c9d0a1823d64a7974d65901bfb98145ccc3987f6
sureshpallapu <pallapusuresh2000@gmail.com>
2025-03-09 22:44:47 +0530

    new

---
+++ JAVAPROGRAMMING/src/ARRAYPROGRAMS/ReverseArray.java
@@ -0,0 +1,13 @@
+package ARRAYPROGRAMS;
+
+
+// Reverse an Array
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Reversed array:");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
